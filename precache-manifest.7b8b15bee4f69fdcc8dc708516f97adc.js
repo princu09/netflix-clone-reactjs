63fbce16da9f47287fb936b790245be0951fdb5a
Prince Patel <neverusedrive@gmail.com>
2022-04-01 17:32:38 +0530

    Updates

---
+++ precache-manifest.7b8b15bee4f69fdcc8dc708516f97adc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9bd3ff546f510cf1fec",
+    "url": "/netflix-clone-reactjs/static/css/main.f94085d3.chunk.css"
+  },
+  {
+    "revision": "d9bd3ff546f510cf1fec",
+    "url": "/netflix-clone-reactjs/static/js/main.d9bd3ff5.chunk.js"
+  },
+  {
+    "revision": "4e9aafb99959cf9fe670",
+    "url": "/netflix-clone-reactjs/static/js/1.4e9aafb9.chunk.js"
+  },
+  {
+    "revision": "d974994bad46e18b4b3c",
+    "url": "/netflix-clone-reactjs/static/js/2.d974994b.chunk.js"
+  },
+  {
+    "revision": "b44d2a90a02351cf0a74",
+    "url": "/netflix-clone-reactjs/static/js/runtime~main.b44d2a90.js"
+  },
+  {
+    "revision": "c2b22f59e08f75a3abae25e4b5519869",
+    "url": "/netflix-clone-reactjs/index.html"
+  }
+];
